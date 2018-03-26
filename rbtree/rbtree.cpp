61c16a2e2a34b87cf2be7d2fa8600636c4418ef7
fyman.zhang <fyman.zhang@icloud.com>
2018-03-26 22:21:59 +0800

    update

--- rbtree/rbtree.cpp
+++ rbtree/rbtree.cpp
@@ -7,31 +7,67 @@
 using std::cout;
 using std::endl;
 
-class AVLNode
+/*
+	           (A)        red/black tree rules:
+1. root is black
+2. nil leaf is black
+3. node is either black or red
+4. no consecutive red node
+5. all path from root to nil leaf have the same black depth
+
+			   (B)        red/black tree insert rules
+1. always insert red node
+
+2. if insert root node, root=>black -> done
+
+3. if parent(black) -> done
+
+4. if parent(red)&&uncle(red)
+   parent=>black and grandpa=>red, recheck grand parent, push black to bottom untill the root
+   
+5. if parent(red)&&uncle(black)
+   5a: parent(left)&&node(left): 
+  i.  rightRotate
+  ii. recolor:  parent=>black, children=>red
+
+   5b: parent(right)&&node(right):
+  i.  leftRotate
+  ii. recolor: parent=>black, children=>red
+  
+   5c: parent(left)&&node(right):
+  i. leftRotate + rightRotate
+  ii. recolor: parent=>black, children=>red
+
+   5d: parent(right)&&node(left):
+  i. rightRotate + leftRotate
+  ii. recolor: parent=>black, children=>red   
+*/
+
+class RBNode
 {
 public:
-  AVLNode(int key)  : key_(key), left_(nullptr), right_(nullptr) {};
+  RBNode(int key)  : key_(key), left_(nullptr), right_(nullptr), black_(false) {};
   
-  ~AVLNode() {
+  ~RBNode() {
     left_ = nullptr;
     right_ = nullptr;
   };
   int key_;
-  int height_;
-  AVLNode *left_;
-  AVLNode *right_;
+  bool black_;
+  RBNode *left_;
+  RBNode *right_;
 };
 
-class AVL
+class RBTree
 {
 public:
-  AVL() :root_(nullptr){};
+  RBTree() :root_(nullptr){};
   
-  ~AVL() {
+  ~RBTree() {
     Delete(root_);
   };
 
-  void Delete(AVLNode *node) {
+  void Delete(RBNode *node) {
     if(node){
       Delete(node->left_);
       Delete(node->right_);
@@ -39,22 +75,22 @@ public:
     }
   }
 
-  AVLNode *Insert(int key, AVLNode *parent) {
-    AVLNode *root = nullptr;
+  RBNode *Insert(int key, RBNode *parent) {
+    RBNode *root = nullptr;
     if(!parent) {
-      root = new AVLNode(key);
+      root = new RBNode(key);
     } else if(key < parent->key_) {
       if(parent->left_) {
         parent->left_ = Insert(key, parent->left_);
       } else {
-        parent->left_ = new AVLNode(key);
+        parent->left_ = new RBNode(key);
         cout<< "[" << key << "]" << "->"<< "[" << parent->key_ << "]" <<  endl;
       }
     } else {
       if(parent->right_) {
         parent->right_ = Insert(key, parent->right_);
       } else {
-        parent->right_ = new AVLNode(key);
+        parent->right_ = new RBNode(key);
         cout<<"[" << parent->key_ <<"]" << "<-" << "[" << key << "]" << endl;
       }
     }
@@ -83,7 +119,7 @@ public:
 	return root;
   }
 
-  int _Diff(AVLNode *parent) {
+  int _Diff(RBNode *parent) {
     int right = _Height(parent->right_);
     int left = _Height(parent->left_);
     int diff = left - right;
@@ -91,31 +127,31 @@ public:
     return diff;
   }
   
-  AVLNode *_LeftRotate(AVLNode *node) { // right rotation once!
+  RBNode *_LeftRotate(RBNode *node) { // right rotation once!
     cout << "_LeftRotate["<<node->key_<<"]" << endl;
-    AVLNode *root = node->right_;
+    RBNode *root = node->right_;
     node->right_ = root->left_;
     root->left_ = node;
     return root;
   }
   
-  AVLNode *_RightRotate(AVLNode *node) { // right rotation once!
+  RBNode *_RightRotate(RBNode *node) { // right rotation once!
     cout << "_RightRotate["<<node->key_<<"]" << endl;
-    AVLNode *root = node->left_;
+    RBNode *root = node->left_;
     node->left_ = root->right_;
     root->right_ = node;
     return root;
   }
 
   
-  AVLNode *_LRRotate(AVLNode *node) { // right rotation once!
+  RBNode *_LRRotate(RBNode *node) { // right rotation once!
     cout << "_LRRotate["<<node->key_<<"]" << endl;
 	node->left_ = _LeftRotate(node->left_);
     return _RightRotate(node);
 	
   }
   
-  AVLNode *_RLRotate(AVLNode *node) { // right rotation once!
+  RBNode *_RLRotate(RBNode *node) { // right rotation once!
     cout << "_RLRotate["<<node->key_<<"]" << endl;
 	node->right_ = _RightRotate(node->right_);
     return _LeftRotate(node);
@@ -126,7 +162,7 @@ public:
     cout << endl;
   }
   
-  void _Traverse(AVLNode *node) {
+  void _Traverse(RBNode *node) {
     if(node) {
       cout << node->key_ << " " << " h:" << _Height(node) << " diff: " << _Diff(node) << endl;
       _Traverse(node->left_);
@@ -142,7 +178,7 @@ public:
     return (x>y?y:x);
   }
 
-  int _Height(AVLNode *node) {
+  int _Height(RBNode *node) {
     int left_height = 0;
     int right_height = 0;
     int height = 0;
@@ -155,19 +191,19 @@ public:
     }
     return height;
   }
-  AVLNode *root_;
+  RBNode *root_;
 };
 
-class AVL_GTest : public ::testing::Test {
+class RBTree_GTest : public ::testing::Test {
 
 protected:
-  AVL *tree_;
+  RBTree *tree_;
 //  int leafs_key_[15] = {55, 23, 91, 20, 25, 80, 120, 15, 21, 24, 28, 70, 85, 110, 150};
 //  int leafs_key_[3] = {30, 10, 20};
   int leafs_key_[11] = {20, 10, 40, 30, 50, 60, 70, 160, 150, 25, 28};
 
   virtual void SetUp(){
-  tree_ = new AVL();
+  tree_ = new RBTree();
   }
   
   virtual void TearDown(){
@@ -175,7 +211,7 @@ protected:
   }
 };
 
-TEST_F(AVL_GTest, AVLInsertBalance_GTest){
+TEST_F(RBTree_GTest, RBTreeInsertBalance_GTest){
   for(unsigned int i = 0; i < (sizeof(leafs_key_)/sizeof(leafs_key_[0])); i ++) {
 	tree_->root_ = tree_->Insert(leafs_key_[i], tree_->root_);
   }
