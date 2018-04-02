94d7864059662e2c3298f364b26653c185ad1cac
fyman.zhang <fyman.zhang@icloud.com>
2018-04-02 10:20:18 +0800

    modify delete node

--- bstree/bstree.cpp
+++ bstree/bstree.cpp
@@ -49,8 +49,8 @@ public:
   ~Node() {
     left_ = nullptr;
     right_ = nullptr;
-		key_ = 0;
-		ofs_ = 0;
+    key_ = 0;
+    ofs_ = 0;
   };
   int key_;
   Node *left_;
@@ -81,157 +81,163 @@ public:
   
   bool Delete(int key) {
     if(!root_) {
-		  cout << "Delete failed on a empty tree" << endl;
-		  return false;
+      cout << "Delete failed on a empty tree" << endl;
+      return false;
     }
-		Node *del_node = _Delete(key, root_);
-		if(del_node)
-    	return true;
-		else
-			return false;
+    Node *del_node = _Delete(key, root_, nullptr);
+    if(del_node)
+      return true;
+    else
+      return false;
   }
 
   void TraverseInOrder() {
+    cout << "In Order:" << endl;
     _InTravse(root_);
     cout << endl;
   }
   
   void TraversePreOrder() {
+    cout << "Pre Order:" << endl;
     _PreTravse(root_);
     cout << endl;
   }
   
   void TraversePostOrder() {
+    cout << "Post Order:" << endl;
     _PostTravse(root_);
     cout << endl;
   }
 	
-	void TraverseTopBottom() {
-		int height_pre = Height(root_);
-		int height_cur = height_pre;
-		root_->ofs_ = height_cur;
-	  node_queue_.empty();
-		node_queue_.push(root_);
-		START();
-		while(node_queue_.size()) {
-			Node *cur = node_queue_.front();
-			height_cur = Height(cur);
-		  //cout << cur->key_ << ": " << height_cur << endl;
-			if(cur && cur->left_) {
-				cur->left_->ofs_ = cur->ofs_ - 1;
-				node_queue_.push(cur->left_);
-			}
-			if(cur && cur->right_) {
-				cur->right_->ofs_ = cur->ofs_ + 1;
-				node_queue_.push(cur->right_);
-			}
-			cout << cur->key_ << " ";
-			//cout << "[" << cur->ofs_ << "]" << " ";
-			OFFSET(cur->ofs_);
-			node_queue_.pop();
-			if(height_pre != height_cur) {
-				NETLINE();
-				START();
-				height_pre = height_cur;
-			}
-		}
-		cout << endl;
-	}
+  void TraverseTopBottom() {
+    cout << "Top Bottom:" << endl;
+    int height_pre = Height(root_);
+    int height_cur = height_pre;
+    root_->ofs_ = height_cur;
+    node_queue_.empty();
+    node_queue_.push(root_);
+    START();
+    while(node_queue_.size()) {
+      Node *cur = node_queue_.front();
+      height_cur = Height(cur);
+      //cout << cur->key_ << ": " << height_cur << endl;
+      if(cur && cur->left_) {
+        cur->left_->ofs_ = cur->ofs_ - 1;
+        node_queue_.push(cur->left_);
+      }
+      if(cur && cur->right_) {
+        cur->right_->ofs_ = cur->ofs_ + 1;
+        node_queue_.push(cur->right_);
+      }
+      cout << cur->key_ << " ";
+      //cout << "[" << cur->ofs_ << "]" << " ";
+      OFFSET(cur->ofs_);
+      node_queue_.pop();
+      if(height_pre != height_cur) {
+        NETLINE();
+        START();
+        height_pre = height_cur;
+      }
+    }
+    cout << endl;
+  }
 
-	int Height(Node *parent) {
-	  int height = 0;
-		int height_left = 0;
-		int height_right = 0;
-		
-	  if(!parent) {
-			height = 1;
-	  } else {
-	    height_left = Height(parent->left_);
-			height_right = Height(parent->right_);
-			height = MAX(height_left, height_right) + 1;
-	  }
-		return height;
-	}
+  int Height(Node *parent) {
+    int height = 0;
+    int height_left = 0;
+    int height_right = 0;
+  
+    if(!parent) {
+      height = 1;
+    } else {
+      height_left = Height(parent->left_);
+      height_right = Height(parent->right_);
+      height = MAX(height_left, height_right) + 1;
+    }
+    return height;
+  }
 	
 private:
   Node *_Insert(int key, Node *parent) {
-		if(key == parent->key_) {
-			
-		} else if(key < parent->key_) {
-		  if(parent->left_) {
-				parent->left_ = _Insert(key, parent->left_);
-		  } else {
-		    parent->left_ = new Node(key);
-				cout << parent->left_->key_ << " <- " << parent->key_ << endl;
-		  }
-		} else {
-			if(parent->right_) {
-				parent->right_ = _Insert(key, parent->right_);
-			} else {
-				parent->right_ = new Node(key);
-				cout << parent->key_ << " -> " << parent->right_->key_  <<  endl;
-			}
-		}
-		
-		return parent;
+    if(key == parent->key_) {
+    
+    } else if(key < parent->key_) {
+      if(parent->left_) {
+        parent->left_ = _Insert(key, parent->left_);
+      } else {
+        parent->left_ = new Node(key);
+        cout << parent->left_->key_ << " <- " << parent->key_ << endl;
+      }
+    } else {
+      if(parent->right_) {
+        parent->right_ = _Insert(key, parent->right_);
+      } else {
+        parent->right_ = new Node(key);
+        cout << parent->key_ << " -> " << parent->right_->key_  <<  endl;
+      }
+    }
+    
+    return parent;
   }
   
-  Node *_Delete(int key, Node *parent) {
+  Node *_Delete(int key, Node *parent, Node *grandpa) {
     Node *del_node = nullptr;
-  	if(key == parent->key_) {
-			cout << "Binggo => " << key << endl;
-			Node *swap_node = _LeftMostNode(parent);
-			if(swap_node == parent) { // no left subtree
-				cout << swap_node->key_ << " has no left subtree ";
-				swap_node = _RightMostNode(parent); 
-				if(swap_node == parent) { // no left and no right and subtree, it's a leaf node, just delete the leaf.
-					cout << ",and no right subtree neither." << endl;
-					delete parent;
-					del_node = parent;
-					return del_node;
-				}
-			}
-			parent->key_ = swap_node->key_;
+    if(key == parent->key_) {
+      bool left = true;
+      cout << "Binggo => " << key << endl;
+      Node *swap_node = _LeftMostNode(parent);
+      if(swap_node == parent) { // no left subtree
+        left = false;
+        cout << swap_node->key_ << " has no left subtree ";
+        swap_node = _RightMostNode(parent); 
+        if(swap_node == parent) { // no left and no right and subtree, it's a leaf node, just delete the leaf.
+          cout << ",and no right subtree neither." << endl;
+          delete parent;
+          del_node = parent;
+          if(grandpa) { // delete the root node
+            if(grandpa->left_ == parent) {
+              grandpa->left_ = nullptr;
+            } else {
+              grandpa->right_ = nullptr;
+            }
+          } else {
+            root_ = nullptr;
+          }
+          return del_node;
+        }
+      }
+      
 			cout << "swap " << parent->key_ << " with " << swap_node->key_ << endl;
-			Node *end_node = nullptr;
-			if(swap_node->left_) {
-				end_node = swap_node->left_;
-				swap_node->left_ = nullptr;
-			} else if(swap_node->right_) {
-				end_node = swap_node->right_;
-				swap_node->right_ = nullptr;
-			}
-			if(end_node) {
-				cout << "end wrap " << swap_node->key_ << " with " << end_node->key_ << endl;
-				swap_node->key_ = end_node->key_;
-				delete end_node;
-			} else {
-				delete swap_node;
-			}
-			del_node = parent; 
-  	} else if(key < parent->key_){
-			if(parent->left_) {
-				del_node = _Delete(key, parent->left_);
-			}
-  	} else {
-			if(parent->right_) {
-				del_node = _Delete(key, parent->right_);
-			}
-  	}
+			cout << "swap to del " << swap_node->key_ << endl;
+      parent->key_ = swap_node->key_;
+      if(left) {
+        del_node = _Delete(swap_node->key_, parent->left_, parent);
+      } else {
+        del_node = _Delete(key, parent->right_, parent);
+      }
+    } else if(key < parent->key_) {
+      if(parent->left_) {
+        del_node = _Delete(key, parent->left_, parent);
+      }
+    } else {
+      if(parent->right_) {
+        del_node = _Delete(key, parent->right_, parent);
+      }
+    }
     return del_node;
   }
 
-	void _Delete(Node *parent) {
-		if(parent->left_) {
-			_Delete(parent->left_);
-		}
-		if(parent->right_) {
-			_Delete(parent->right_);
-		}
-		if(!(parent->left_)&&!(parent->right_)) {
-			delete parent;
-		}
-	}
+  void _Delete(Node *parent) {
+    if(parent->left_) {
+      _Delete(parent->left_);
+    }
+    if(parent->right_) {
+      _Delete(parent->right_);
+    }
+    if(!(parent->left_)&&!(parent->right_)) {
+      delete parent;
+    }
+  }
 	
   void _InTravse(Node *parent) {
     if(parent) {
@@ -243,7 +249,7 @@ private:
   
   void _PreTravse(Node *parent) {
     if(parent) {
-	  	cout << parent->key_ << " ";
+      cout << parent->key_ << " ";
       _PreTravse(parent->left_);
       _PreTravse(parent->right_);
     }
@@ -283,10 +289,10 @@ protected:
   BSTree *tree_;
   int leafs_key_[7] = {100, 50, 250, 20, 70, 200, 300};
   virtual void SetUp(){
-		tree_ = new BSTree();
-	  for(unsigned int i = 0; i < (sizeof(leafs_key_)/sizeof(leafs_key_[0])); i ++) {
-			tree_->Insert(leafs_key_[i]);
-	  }
+    tree_ = new BSTree();
+    for(unsigned int i = 0; i < (sizeof(leafs_key_)/sizeof(leafs_key_[0])); i ++) {
+      tree_->Insert(leafs_key_[i]);
+    }
   }
   
   virtual void TearDown(){
@@ -295,30 +301,30 @@ protected:
 };
 
 TEST_F(BSTree_GTest, TraverseTopBottom_GTest){
-	tree_->TraversePreOrder();
+  tree_->TraversePreOrder();
   tree_->TraverseTopBottom();
 }
 
 TEST_F(BSTree_GTest, DeleteNode_GTest){
-	int input;
-	bool ret = false;
-	do {
-		cout << "Please input node key to delete: " << endl;
-		cin >> input;
-		ret = tree_->Delete(input);
-		if(!ret) {
-			cout << "Delete " << input << " failed!" << endl;
-		} else {
-			cout << "Delete " << input << " Success!" << endl;
-		}
-		tree_->TraversePreOrder();
-		tree_->TraverseTopBottom();
-	}while(ret);
+  int input;
+  bool ret = false;
+  do {
+    cout << "Please input node key to delete: " << endl;
+    cin >> input;
+    ret = tree_->Delete(input);
+    if(!ret) {
+      cout << "Delete " << input << " failed!" << endl;
+    } else {
+      cout << "Delete " << input << " Success!" << endl;
+    }
+    tree_->TraversePreOrder();
+    tree_->TraverseTopBottom();
+  }while(ret);
 }
 
 
 int main(int argc, char *argv[])
 {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
